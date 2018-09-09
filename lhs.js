7e5a3d4390ea5e887d56b0f6996f148f43450a78
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-09-09 19:20:53 +0300

    Add all manga information scraper

--- lhs.js
+++ lhs.js
@@ -2,6 +2,12 @@ const request = require('requestretry').defaults({json: true});
 const { lhscans } = require('./config.json');
 const cheerio = require('cheerio');
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
 async function getMangaTitles(){
     let mangas = [];
     let response = await request({
@@ -16,14 +22,41 @@ async function getMangaTitles(){
 	mangas.push(
 	    {
 		name: $(elem).text(),
-		url: $(elem).attr("href")
+		url: $(elem).attr("href"),
+		chapters: []
+	    });
+    });
+    return mangas;
+}
+
+async function addChapter(){
+    let mangas = await getMangaTitles();
+    // Now dirty stuff starts
+    await asyncForEach(mangas, async(manga, index) => {
+	//console.log(manga);
+	let response = await request({
+	    url: lhscans + manga.url,
+	    method: 'GET',
+	    maxAttempts: 1,
+	    retryDelay: 5000,
+	    retryStrategy: request.RetryStrategies.HTTPOrNetworkError
+	});
+	const $ = cheerio.load(response.body);
+	$(response.body).find("#list-chapters p .titleLink a")
+	    .each((index, elem) => {
+		mangas[index].chapters.push(
+		    {
+			chapterNo: $(elem).attr("title"),
+			chapterUrl:  $(elem).attr("href")
+		    }
+		);
 	    });
     });
     return mangas;
 }
 
 async function printer() {
-    let x = await getMangaTitles();
+    let x = await addChapter();
     console.log(x);
 }
 
